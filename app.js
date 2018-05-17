17e6dfd723e1272cb843fcf91ae241a8f5c629c5
xhoi.luli <xhoi.luli@mudano.com>
2018-05-17 15:29:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//INTRO TO JS UNIT TESTING WITH MOCHA JS & CHAI
+//https://www.youtube.com/watch?v=MLTRHc5dk6s
+
+//PART 1 UP TO 8.48
+// module.exports = function(){
+//
+// return 'hello';
+//
+// }
+
+//PART 2
+module.exports = {
+  sayHello: () => {
+    return "hello";
+  },
+  addNumbers: (value1, value2) => {
+    return value1 + value2;
+  }
+};
---
+++ test/appTest.js
@@ -0,0 +1,35 @@
+//const assert = require('assert') DEFAULT NODEJS ASSERTION module
+const assert = require("chai").assert;
+//const app = require('../app'); Part 1
+// const sayHello = require("../app").sayHello;
+// const addNumbers = require("../app").addNumbers;
+const app = require("../app");
+
+//Results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5, 5);
+
+describe("App", () => {
+  describe("sayHello()", () => {
+    it("sayHello should return hello", () => {
+      // let result = app.sayHello();
+      assert.equal(sayHelloResult, "hello");
+    });
+
+    it("sayHello should return type string", () => {
+      // let result = app.sayHello();
+      assert.typeOf(sayHelloResult, "string");
+    });
+  });
+  describe("addNumbers()", () => {
+    it("addNumbers should be above 5", () => {
+      // let result = app.addNumbers(4, 3);
+      assert.isAbove(addNumbersResult, 5);
+    });
+
+    it("addNumbers should return type number", () => {
+      // let result = app.addNumbers(4, 3);
+      assert.typeOf(addNumbersResult, "number");
+    });
+  });
+});
